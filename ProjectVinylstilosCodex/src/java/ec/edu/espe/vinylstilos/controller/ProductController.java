084339852efe26b8fdeec94740a2b62a2593e0d2
pacadena5 <pacadena5@espe.edu.ec>
2021-06-22 20:11:37 -0500

    restNameProduct

--- ProjectVinylstilosCodex/src/java/ec/edu/espe/vinylstilos/controller/ProductController.java
+++ ProjectVinylstilosCodex/src/java/ec/edu/espe/vinylstilos/controller/ProductController.java
@@ -44,4 +44,5 @@ public class ProductController {
         return dataProduct;
     }
     
+    
  }
---
+++ ProjectVinylstilosCodex/src/java/ec/edu/espe/vinylstilos/webservices/ProductNameResource.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.vinylstilos.webservices;
+
+import ec.edu.espe.vinylstilos.controller.ProductController2;
+import ec.edu.espe.vinylstilos.model.Product;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author pabli
+ */
+@Path("productName")
+public class ProductNameResource {
+
+    private ProductController2 controller = new ProductController2();
+    
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of ProductNameResource
+     */
+    public ProductNameResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of ec.edu.espe.vinylstilos.webservices.ProductNameResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Product getProductByName(@PathParam("name") String name ) {
+        return controller.getDataByName(name);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void postProduct(Product prod ) {
+        controller.postProduct(prod);
+    }
+    /**
+     * PUT method for updating or creating an instance of ProductNameResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
