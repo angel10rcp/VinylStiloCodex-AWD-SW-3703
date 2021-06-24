0f7fb1364235eb1ff483715d91c769207bb97e91
KevinCaicedo1 <kevincaicedo143@gmail.com>
2021-06-23 20:18:21 -0500

    add function UserRest

--- ProjectVinylstilosCodex/src/java/ec/edu/espe/vinylstilos/webservices/UserRest.java
+++ ProjectVinylstilosCodex/src/java/ec/edu/espe/vinylstilos/webservices/UserRest.java
@@ -5,13 +5,19 @@
  */
 package ec.edu.espe.vinylstilos.webservices;
 
+import ec.edu.espe.vinylstilos.controller.UserController;
+import ec.edu.espe.vinylstilos.model.User;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -24,6 +30,7 @@ public class UserRest {
 
     @Context
     private UriInfo context;
+    private UserController userController = new UserController();
 
     /**
      * Creates a new instance of UserRest
@@ -32,22 +39,45 @@ public class UserRest {
     }
 
     /**
-     * Retrieves representation of an instance of ec.edu.espe.vinylstilos.webservices.UserRest
+     * Retrieves representation of an instance of restVinilo.UserResource
+     *
      * @return an instance of java.lang.String
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public List<User> getJson() {
+        return userController.ShowUsers();
     }
 
     /**
-     * PUT method for updating or creating an instance of UserRest
+     * PUT method for updating or creating an instance of UserResource
+     *
      * @param content representation for the resource
      */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void create(User user) {
+        userController.insertUser(user);
     }
+
+    @PUT
+    @Path("{idUser}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("idUser") String id, User entity) {
+        userController.putUser(id, entity);
+    }
+
+    @DELETE
+    @Path("{idUser}")
+    public void remove(@PathParam("idUser") String id) {
+        userController.deleteUser(id);
+    }
+
+    @GET
+    @Path("{idUser}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public User find(@PathParam("idUser") String id) {
+        return userController.getUserById(id);
+    }
+
 }
